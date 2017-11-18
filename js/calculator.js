6f002286185e27f26263d4130fbc75ee20353449
Promie Yutasane <promie.yutasane@gmail.com>
2017-11-19 07:51:43 +1100

    Pseudo code logic/algorithm drafted

---
+++ js/calculator.js
@@ -0,0 +1,32 @@
+//Eval() function
+
+
+/*
+
+Function #1
+Logic for On and Off Button
+
+
+Function #2
+Get the numbers to appear on the screen
+- onclick function that takes in one parameter with the value to the screen
+- IF value is equal to AC = set value to zero
+
+
+Function #3
+Get total value when press equal
+- eval to the value of the display (eval() function)
+
+
+Functionn #4
+Back space, the CE button.
+
+Things to consider:
+- What happens when a user presses and Operator first?
+- What happens when the user presses the Operator twice?
+- Working out the length of the digits
+
+
+
+
+*/
