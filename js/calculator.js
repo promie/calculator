dd6fd539e2756b0c51beffbf9c166548b534233b
Promie Yutasane <promie.yutasane@gmail.com>
2017-11-19 13:18:33 +1100

    First Function working

--- js/calculator.js
+++ js/calculator.js
@@ -13,6 +13,8 @@ Get the numbers to appear on the screen
 - IF value is equal to AC = set value to zero
 
 
+
+
 Function #3
 Get total value when press equal
 - eval to the value of the display (eval() function)
@@ -26,7 +28,50 @@ Things to consider:
 - What happens when the user presses the Operator twice?
 - Working out the length of the digits
 
-
-
-
 */
+
+/*
+Function #2
+Get the numbers to appear on the screen
+- onclick function that takes in one parameter with the value to the screen
+- IF value is equal to AC = set value to zero
+*/
+
+
+const screenValue = document.getElementById('numbers');
+
+
+
+const displayScreen = (number) =>{
+    
+    if(screenValue.value.length < 10){
+        return screenValue.value += number;
+    }
+}
+
+const clearScreen = () =>{
+    return screenValue.value = '';
+}
+
+const total = () =>{
+
+    const number = screenValue.value;
+    const newTotal = eval(number);
+
+    screenValue.value = newTotal;
+
+}
+
+const deleteLastVal = () =>{
+
+    const displayValue = screenValue.value;
+    const len = displayValue.length - 1;
+    const newValue = displayValue.substring(0, len);
+
+    screenValue.value = newValue;
+
+}
+
+
+
+
