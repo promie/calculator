34321dba8507420b3ed9665204681b5fdbd0f32e
Promie Yutasane <promie@greenguys.local>
2017-11-21 09:22:24 +1100

    Refactor v2.0

--- js/calculator.js
+++ js/calculator.js
@@ -1,38 +1,5 @@
-//Eval() function
-
-
-/*
-
-Function #1
-Logic for On and Off Button
-
-
-Function #2
-Get the numbers to appear on the screen
-- onclick function that takes in one parameter with the value to the screen
-- IF value is equal to AC = set value to zero
-
-
-yes HAHAHAHA
-Function #3
-Get total value when press equal
-- eval to the value of the display (eval() function)
-
-
-Things to consider:
-- What happens when a user presses and Operator first?
-- What happens when the user presses the Operator twice?
-- Working out the length of the digits
-
-*/
-
-
-//const screenValue = document.getElementById('numbers');
-//let totalValue = '';
-//const maxLength = 10;
-
-var screenValue = ""
-var calculator = []
+let screenValue = ""
+let calculator = []
 
 
 const displayScreen = (str) => {
@@ -40,14 +7,17 @@ const displayScreen = (str) => {
 }
 
 const handleOperator = (operator) => {
-    calculator.push(screenValue, operator)
-    screenValue = ""
-    console.log(calculator)
+    if(screenValue.length < 10){
+        calculator.push(screenValue, operator)
+        screenValue = ""    
+    }
 }
 
 const handleNumber = (number) => {
-    screenValue += number
-    displayScreen(screenValue)
+    if(screenValue.length < 10){
+        screenValue += number
+        displayScreen(screenValue)
+    }
 }
 
 const total = () => {
@@ -55,55 +25,23 @@ const total = () => {
     displayScreen(eval(calculator.join('')))
 }
 
-/*
-const displayScreen = (number) =>{
-    
-    if(number === '+' || number === '-' || number ==='*' || number === '/' ){
-        return totalValue += number;
-        
-    }
-
-    if(screenValue.value.length < maxLength){
-        return screenValue.value += number;
-    }
-}
-
-
 const clearScreen = () =>{
-    return screenValue.value = '';
-}
-
-const total = () =>{
-
-    const number = screenValue.value;
-    const newTotal = eval(number);
-
-    screenValue.value = newTotal;
+    
+    document.getElementById('numbers').value = '';
+    screenValue = '';
 
 }
 
 const deleteLastVal = () =>{
-
-    const displayValue = screenValue.value;
+    
+    const displayValue = document.getElementById('numbers').value;
     const len = displayValue.length - 1;
     const newValue = displayValue.substring(0, len);
 
-    screenValue.value = newValue;
-
+    document.getElementById('numbers').value = newValue;
+    screenValue = '';
+    
 }
 
-const onOFF = () =>{
-    
-    let status = document.getElementById('on-off').innerHTML;
-    
-    if(status === 'ON'){
-        screenValue.value = 'WELCOME';
-        status = 'OFF';
-    }   
-    
-
-}
-
-*/
 
 
